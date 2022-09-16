14a90b7c6d1caacb8fad6a4a56f993dab7d03082
Moraj Dhwaj <109418239+morajdhwaj@users.noreply.github.com>
2022-09-16 15:47:02 +0530

    chnages in index .js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ import { GET_POSTS } from "../utils/query";
 
 const index = () => {
   return (
-    <div className="max-w-7xl  bg-[#0d1117]">
+    <div className="max-w-7xl  bg-[#0d1117] jdcbjd">
       <Banner/>
       <Banner2/>
       <Banner3/>
