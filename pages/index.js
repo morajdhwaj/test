1479645ff9400caa5701e6bd26a9612236432142
moraj <dhwajmoraj7@gmail.com>
2022-09-28 15:50:20 +0530

    Change title name.

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,9 @@ import Banner from "../Components/Banner";
 import Banner2 from "../Components/Banner2";
 import Banner3 from "../Components/Banner3";
 import Banner5 from "../Components/Banner5";
-import Blog from "./blog";
 import Banner4 from "../Components/icons/Banner4";
-import { client } from "../utils/client";
-import { GET_POSTS } from "../utils/query";
-import Container6 from "../Components/container6";
-
 import Container7 from "../Components/Container7";
 import NavBar from "../Components/NavBar";
-import Container1 from "../Components/Container1";
 import Studies from "../Components/Studies";
 import Qiuck from "../Components/Qiuck";
 import Footer from "../Components/Footer";
